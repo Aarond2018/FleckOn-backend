b6d1e14a17c94772b7f58283626c55dd01fa112e
Aarond2018 <aarondamilola1998@gmail.com>
2022-04-23 16:45:05 +0100

    feature: implemented post update and get User's places

--- controllers/places-contollers.js
+++ controllers/places-contollers.js
@@ -9,23 +9,27 @@ const cloudinary = require("../util/cloudinary");
 const { default: mongoose } = require("mongoose");
 
 exports.getPlaceById = async (req, res, next) => {
-  let place;
-  try {
-    place = await Place.findById(req.params.id)
-  } catch (error) {
-    return next(new HttpError("An error occurred, Please try again later", 500))
-  }
+	let place;
+	try {
+		place = await Place.findById(req.params.id);
+	} catch (error) {
+		return next(
+			new HttpError("An error occurred, Please try again later", 500)
+		);
+	}
 
-  if(!place) {
-    return next(new HttpError("Could not find a place for the provided ID", 404))
-  }
+	if (!place) {
+		return next(
+			new HttpError("Could not find a place for the provided ID", 404)
+		);
+	}
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      place
-    }
-  })
+	res.status(200).json({
+		status: "success",
+		data: {
+			place,
+		},
+	});
 };
 
 exports.createPlace = async (req, res, next) => {
@@ -81,46 +85,122 @@ exports.createPlace = async (req, res, next) => {
 		await user.save({ session: session });
 		await session.commitTransaction();
 	} catch (error) {
-    console.log("yyyy")
+		console.log("yyyy");
 		return next(new HttpError("Creating place failed, Please try again."));
 	}
 
-  res.status(201).json({
-    status: "success",
-    data: {
-      newPlace
+	res.status(201).json({
+		status: "success",
+		data: {
+			newPlace,
+		},
+	});
+};
+
+exports.getPlacesByUserId = async (req, res, next) => {
+	let userWithPlaces;
+	try {
+		userWithPlaces = await User.findById(req.params.id).populate("places");
+	} catch (error) {
+		return next(
+			new HttpError("Fetching places failed, please try again later", 500)
+		);
+	}
+
+	if (!userWithPlaces || userWithPlaces.places.length === 0) {
+		return next(
+			new HttpError("Could not find places for the provided user id.", 404)
+		);
+	}
+
+	res.status(200).json({
+		status: "success",
+		data: {
+      places: userWithPlaces.places
     }
-  })
+	});
 };
 
 exports.deletePlaceById = async (req, res, next) => {
+	let place;
+	try {
+		place = await Place.findById(req.params.id).populate("creator");
+	} catch (error) {
+		return next(
+			new HttpError("An error occurred, could not delete place", 500)
+		);
+	}
+
+	if (!place) {
+		return next(
+			new HttpError("Could not find a place for the provided id", 404)
+		);
+	}
+
+	if (place.creator.id !== req.userData.userId) {
+		return next(new HttpError("You are allowed to delete this place", 401));
+	}
+
+	try {
+		const session = await mongoose.startSession();
+		await session.startTransaction();
+		await place.remove({ session });
+		place.creator.places.pull(place);
+		await place.creator.save({ session });
+		await session.commitTransaction();
+	} catch (error) {
+		return next(
+			new HttpError("Something went wrong, could not delete place.", 500)
+		);
+	}
+
+	res.status(204).json({
+		status: "success",
+	});
+};
+
+
+exports.updatePlace = async (req, res, next) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+		return next(
+			new HttpError("Invalid inputs passed, please check your data", 422)
+		);
+	}
+
+  const {title, description} = req.body
+
   let place;
   try {
-    place = await Place.findById(req.params.id).populate("creator")
+    place = await Place.findById(req.params.id)
   } catch (error) {
-    return next(new HttpError("An error occurred, could not delete place", 500))
+    return next(new HttpError("Something went wrong, try again", 500))
   }
 
-  if (!place) {
-    return next(new HttpError("Could not find a place for the provided id", 404))
+  if(!place) {
+    return next(new HttpError("This place is not recorded in the database", 404))
   }
 
-  if (place.creator.id !== req.userData.userId) {
-    return next(new HttpError("You are allowed to delete this place", 401))
+  if(req.userData.userId !== place.creator.toString()) {
+    console.log(req.userData.userId)
+    console.log(place.creator.toString())
+    return next(new HttpError("You are not allowed to edit this place.", 401))
   }
+
+  place.title = title
+  place.description = description
 
   try {
-    const session = await mongoose.startSession()
-    await session.startTransaction()
-    await place.remove( {session} )
-    place.creator.places.pull(place)
-    await place.creator.save( {session} )
-    await session.commitTransaction()
+    await place.save();
   } catch (error) {
-    return next(new HttpError("Something went wrong, could not delete place.", 500))
+    return next(new HttpError('Something went wrong, could not update place.', 500))
   }
 
-  res.status(204).json({
-    status: "success"
+  res.status(200).json({
+    status: "success",
+    data: {
+      place
+    }
   })
+
 }
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,25 +1,34 @@
-const express = require("express")
-const { check } = require("express-validator")
+const express = require("express");
+const { check } = require("express-validator");
 
-const placesControllers = require("../controllers/places-contollers")
-const checkAuth = require("../middleware/checkAuth")
-const upload = require("../middleware/file-upload")
+const placesControllers = require("../controllers/places-contollers");
+const checkAuth = require("../middleware/checkAuth");
+const upload = require("../middleware/file-upload");
 
-const router = express.Router()
+const router = express.Router();
 
+router.post(
+	"/",
+	checkAuth,
+	upload.single("image"),
+	[
+		check("title").not().isEmpty(),
+		check("description").isLength({ min: 5 }),
+		check("address").not().isEmpty(),
+	],
+	placesControllers.createPlace
+);
 
-router.post("/", checkAuth, upload.single("image"), [
-  check('title')
-    .not()
-    .isEmpty(),
-  check('description').isLength({ min: 5 }),
-  check('address')
-    .not()
-    .isEmpty()
-], placesControllers.createPlace)
+router.get("/:id", placesControllers.getPlaceById);
 
-router.get("/:id", placesControllers.getPlaceById)
+router.delete("/:id", checkAuth, placesControllers.deletePlaceById);
+router.patch(
+	"/:id",
+	checkAuth,
+	[check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+	placesControllers.updatePlace
+);
 
-router.delete("/:id", checkAuth,placesControllers.deletePlaceById)
+router.get("/user/:id", placesControllers.getPlacesByUserId);
 
-module.exports = router
+module.exports = router;
